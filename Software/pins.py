5e7606b313dce9784e9f111668d0f3df9312474c
PEDRO00100 <accounts.pedro@pm.me>
2026-07-08 18:31:21 -0600

    corregi la definicion de pines GP

--- Software/pins.py
+++ Software/pins.py
@@ -18,14 +18,14 @@ button1 = Debouncer(_button1_pin)
 
 # Binary DIP Switch (CFG A6H-4101) - 4-bit decoding (0-15)
 # Active-Low: Switch ON = GND (Bit 1), Switch OFF = Floating (Bit 0)
-payload1Pin = DigitalInOut(GP4)
+payload1Pin = DigitalInOut(GP2)
 payload1Pin.switch_to_input(pull=Pull.UP)  # LSB (Bit 0)
 
-payload2Pin = DigitalInOut(GP5)
+payload2Pin = DigitalInOut(GP3)
 payload2Pin.switch_to_input(pull=Pull.UP)  # Bit 1
 
-payload3Pin = DigitalInOut(GP10)
+payload3Pin = DigitalInOut(GP4)
 payload3Pin.switch_to_input(pull=Pull.UP)  # Bit 2
 
-payload4Pin = DigitalInOut(GP11)
+payload4Pin = DigitalInOut(GP5)
 payload4Pin.switch_to_input(pull=Pull.UP)  # MSB (Bit 3)
